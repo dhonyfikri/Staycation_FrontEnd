a47215f5ed8b02238377edabebe39af0c91645e8
dhonyfikri <muhamad.fikri251114@gmail.com>
2022-11-04 09:56:24 +0700

    ulangi lagi

--- src/elements/Form/index.jsx
+++ src/elements/Form/index.jsx
@@ -1,3 +1,3 @@
 export { default as InputNumber } from "./InputNumber";
 export { default as InputDate } from "./InputDate";
-export { default as Breadcrumb } from "./Breadcrumb";
+export { default as Breadcrumb } from "../Breadcrumb";
